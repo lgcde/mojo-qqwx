9d2bee5d55e8bd5782747b547c3e95071186e381
lgcde <34829026+lgcde@users.noreply.github.com>
2018-07-16 21:25:49 +0800

    Create termux_mojo.sh

---
+++ termux_mojo.sh
@@ -0,0 +1,5 @@
+pkg install -y curl wget make clang *YAML* perl openssl-dev openssl-tool coreutils termux-tools
+termux-setup-storage
+curl http://share-10066126.cos.myqcloud.com/cpanm.pl|perl - App::cpanminus
+cpanm Mojo::Webqq Mojo::Weixin Webqq::Encryption
+wget -P /storage/emulated/0/ https://raw.githubusercontent.com/lgcde/mojo-qqwx/master/qq.pl https://raw.githubusercontent.com/lgcde/mojo-qqwx/master/wx.pl
