91d47655dd6fe57d34ddde1a1464ea89db80a6df
lgcde <34829026+lgcde@users.noreply.github.com>
2018-07-17 00:09:29 +0800

    Update termux_mojo.sh

--- termux_mojo.sh
+++ termux_mojo.sh
@@ -1,5 +1,6 @@
-pkg install -y wget make clang *YAML* perl openssl-dev openssl-tool coreutils termux-tools
+pkg install -y termux-tools
 termux-setup-storage
+pkg install -y wget make clang *YAML* perl openssl-dev openssl-tool coreutils
 curl http://share-10066126.cos.myqcloud.com/cpanm.pl|perl - App::cpanminus
-cpanm Mojo::Webqq Mojo::Weixin Webqq::Encryption
+cpanm --mirror http://mirrors.163.com/cpan/ Mojo::Webqq Mojo::Weixin Webqq::Encryption
 wget -P /storage/emulated/0/ https://raw.githubusercontent.com/lgcde/mojo-qqwx/master/qq.pl https://raw.githubusercontent.com/lgcde/mojo-qqwx/master/wx.pl
